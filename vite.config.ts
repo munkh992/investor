9a67ca9724c2051c2794a4782fa7bc0a8c965023
munkh992 <112835994+munkh992@users.noreply.github.com>
2025-07-31 16:05:44 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tdb-investor-relations/',
+  base: '/investor/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
